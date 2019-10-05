e3b47b55fbdb6994717d6fb4dbc75a850d7858e8
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-05 05:41:41 +0100

    initial commit

---
+++ Assets/Scripts/SelectionItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectionItem : MonoBehaviour
+{    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MouseEntered()
+    {
+        transform.localScale = new Vector3(1.1f,1.1f,1.1f);
+    }
+
+    public void MouseLeft()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
---
+++ Assets/Scripts/ToolsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ToolsManager : MonoBehaviour
+{
+    GameObject selectedTool = null;
+    public TextMeshProUGUI testText;
+
+    public Transform toolsPanel;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectedTool != null) {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedTool.transform.position = new Vector3(pos.x,pos.y,0f);
+
+            if (Input.GetMouseButtonDown(0)) {
+                selectedTool = null;
+            }
+        }
+    }
+
+
+    public void ToolClicked(string name)
+    {
+        Debug.Log(name);
+        GameObject prefab = Resources.Load<GameObject>($"Tools/{name}");
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        selectedTool = Instantiate(prefab,new Vector3(pos.x,pos.y,0f),Quaternion.identity);
+    }
+}
