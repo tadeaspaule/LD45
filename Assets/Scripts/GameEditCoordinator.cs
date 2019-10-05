1c89c0a4fdc50878b7146bb76496862837bc596b
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-05 13:53:33 +0100

    switch between game and edit mode

---
+++ Assets/Scripts/GameEditCoordinator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameEditCoordinator : MonoBehaviour
+{
+    public GameManager gameManager;
+    public EditorManager editorManager;
+    public Animation toolPanelAnims;
+    public TextMeshProUGUI btnText;
+    bool isEditing = true;
+
+    public void ClickedSwitchButton()
+    {
+        if (isEditing) {
+            // switching to game mode
+            editorManager.gameObject.SetActive(false);
+            gameManager.gameObject.SetActive(true);
+            toolPanelAnims.Play("closetools");
+            btnText.text = "Back to editing";
+        }
+        else {
+            // switching to edit mode
+            editorManager.gameObject.SetActive(true);
+            gameManager.gameObject.SetActive(false);
+            toolPanelAnims.Play("opentools");
+            btnText.text = "Play the level";
+        }
+        isEditing = !isEditing;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("end")) {
+            Debug.Log("Reached the end");
+        }
+    }
+}
