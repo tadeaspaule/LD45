b6225e6063142c46b7937f6e3a82ae048ae5266e
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-06 06:17:12 +0100

    some refactoring and better color management

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorManager : MonoBehaviour
+{
+    public Color backgroundColor;
+    public Color accentColor;
+
+    public Image startScreen;
+    public Image endScreen;
+    public Image transitionPanel;
+
+    public Image timerFilled;
+
+    void Start()
+    {
+        startScreen.color = backgroundColor;
+        endScreen.color = backgroundColor;
+        transitionPanel.color = backgroundColor;
+        Camera.main.backgroundColor = backgroundColor;
+
+        timerFilled.color = accentColor;
+    }
+}
