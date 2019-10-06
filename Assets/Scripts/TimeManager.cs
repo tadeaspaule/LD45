1288531ecfd39477ceb3d3a41b7180c8192b33e2
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-06 04:48:12 +0100

    timer working

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,23 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
+    float startTime;
     public float currentTime;
     float endTime;
+    public float multiplier = 1f;
+
+    public TextMeshProUGUI secondsText;
+    public Image clockImage;
+
+    public void SetMultiplier(float m)
+    {
+        multiplier = m;
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
         currentTime = Time.time;
-        endTime = Time.time + 50 * 60;
+        endTime = Time.time + 15 * 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime += Time.deltaTime;
+        currentTime += Time.deltaTime*multiplier;
+        secondsText.text = GetSecondsLeft().ToString();
+        clockImage.fillAmount = 1 - (currentTime-startTime) / (endTime-startTime);
     }
 
     public int GetSecondsLeft()
