b0678f878c577db7f77d93d69e025c6db04671ae
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-06 06:42:26 +0100

    basic start animation

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -6,13 +6,23 @@ using TMPro;
 
 public class TimeManager : MonoBehaviour
 {
+    public EditorManager editorManager;
+    
     float startTime;
     public float currentTime;
     float endTime;
     public float multiplier = 1f;
 
+    bool measuringTime = false;
+
     public TextMeshProUGUI secondsText;
     public Image clockImage;
+
+    // start sequence related
+    public AnimationClip startAnim;
+    public TextMeshProUGUI startSecondsTxt;
+
+    const int timeLimit = 15 * 60;
 
     public void SetMultiplier(float m)
     {
@@ -23,16 +33,33 @@ public class TimeManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startSecondsTxt.text = timeLimit.ToString();
+        StartCoroutine(StartCountdown());
+        StartCoroutine(Setup());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        yield return new WaitForSeconds(startAnim.length - 2.5f);
+        measuringTime = true;
         startTime = Time.time;
         currentTime = Time.time;
-        endTime = Time.time + 15 * 60;
+        endTime = Time.time + timeLimit;
+    }
+
+    IEnumerator Setup()
+    {
+        yield return new WaitForSeconds(startAnim.length + 0.5f);
+        editorManager.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!measuringTime) return;
         currentTime += Time.deltaTime*multiplier;
         secondsText.text = GetSecondsLeft().ToString();
+        startSecondsTxt.text = GetSecondsLeft().ToString();
         clockImage.fillAmount = 1 - (currentTime-startTime) / (endTime-startTime);
     }
 
