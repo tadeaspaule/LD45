7d81567195469e0e99f9f19d376511b54ddf2ba2
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-06 15:33:09 +0100

    added exec

--- Assets/Scripts/EditorManager.cs
+++ Assets/Scripts/EditorManager.cs
@@ -253,7 +253,7 @@ public class EditorManager : MonoBehaviour
         currentScene = menuScene;
         stageList = JsonReader.readJsonArray<Stage>(stagesJson.ToString());
         currentStage = 0;
-        StartEverything(true);        
+        StartEverything(false);        
     }
 
     void StartEverything(bool isDebug)
